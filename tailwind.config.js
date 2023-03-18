08baae30bea07e04dc6abcf0dd699f45e7cf71ed
Cypher Moon <temperaturecaleb@gmail.com>
2023-03-18 20:06:42 +0100

    setup color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,25 @@ module.exports = {
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "xamarin-primary": "#F54E5E",
+        primary: {
+          50: "#FAFCFF",
+          100: "#E1F0FF",
+          DEFAULT: "#0582FD"
+        },
+        heading: "#001A33",
+        gray: {
+          50: " #E9EBED",
+          100: "#CDD1D6",
+          200: "#B1B8BF",
+          DEFAULT: "#798591",
+          600: "#4A525A",
+          800: "#1B1F21"
+        }
+      }
+    },
   },
   plugins: [],
 }
